1ed1b022f977ca5942af2c0fe977e6987df10eca
Alexey Zakhlestin <indeyets@gmail.com>
2009-06-30 16:45:22 +0400

    moved tests to separate path, switched to autoload

---
+++ autoload.php
@@ -0,0 +1,26 @@
+<?php
+
+function TwitterData_autoload($classname)
+{
+    static $paths = null;
+
+    if (null === $paths) {
+        $root = dirname(__FILE__);
+        $paths = array(
+            'TwitterData_Tuple'                     => $root.'/TwitterData_Tuple.php',
+            'TwitterData_Frame'                     => $root.'/TwitterData_Frame.php',
+            'TwitterData_Message'                   => $root.'/TwitterData_Message.php',
+            'TwitterData_Parser'                    => $root.'/TwitterData_Parser.php',
+            'TwitterData_Parser_CallbackInterface'  => $root.'/TwitterData_Parser.php',
+            'TwitterData_Parser_OOPGenerator'       => $root.'/TwitterData_Parser_OOPGenerator.php',
+            'TwitterData_Parser_ArrayGenerator'     => $root.'/TwitterData_Parser_ArrayGenerator.php',
+            'TwitterData'                           => $root.'/TwitterData.php',
+        );
+    }
+
+    if (isset($paths[$classname])) {
+        require $paths[$classname];
+    }
+}
+
+spl_autoload_register('TwitterData_autoload');
---
+++ tests/TestSerialize.php
@@ -0,0 +1,97 @@
+<?php
+
+error_reporting(E_ALL | E_STRICT);
+
+require_once "PHPUnit/Framework/TestCase.php";
+require dirname(__FILE__).'/../autoload.php';
+
+class TestSerialize extends PHPUnit_Framework_TestCase
+{
+    public function testSimpleTuple()
+    {
+        $tuple = new TwitterData_Tuple('key', 'value');
+        $this->assertEquals('$key value', (string)$tuple);
+    }
+
+    public function testSimpleFrame()
+    {
+        $frame = new TwitterData_Frame('subject', array());
+        $this->assertEquals('subject', (string)$frame);
+
+        $frame = new TwitterData_Frame('', array(
+            new TwitterData_Tuple('key', 'value'),
+            new TwitterData_Tuple('key2', 'value2')
+        ));
+        $this->assertEquals('$key value $key2 value2', (string)$frame);
+    }
+
+    public function testSimpleMessage()
+    {
+        $frame1 = new TwitterData_Frame('', array(
+            new TwitterData_Tuple('key', 'value'),
+            new TwitterData_Tuple('key2', 'value2')
+        ));
+
+        $frame2 = new TwitterData_Frame('Hello, World!', array(
+            new TwitterData_Tuple('key', 'value'),
+            new TwitterData_Tuple('key2', 'value2')
+        ));
+
+        $message = new TwitterData_Message();
+        $message->addFrame($frame1)->addFrame($frame2);
+        $this->assertEquals('$key value $key2 value2$ Hello, World! $key value $key2 value2', (string)$message);
+    }
+
+    public function testTupleKeys()
+    {
+        // first-letter
+        // -> good
+        $good = array_merge(range('a', 'z'), range('A', 'Z'), array('_'));
+        foreach($good as $letter) {
+            try {
+                new TwitterData_Tuple($letter.'key', 'string');
+            } catch (UnexpectedValueException $e) {
+                $this->fail();
+            }
+        }
+
+        // -> bad
+        $bad = array_merge(range('0', '9'), array('-', '.', '>', '#'));
+        foreach($bad as $letter) {
+            try {
+                new TwitterData_Tuple($letter.'key', 'string');
+                $this->fail();
+            } catch (UnexpectedValueException $e) {
+            }
+        }
+
+        // correct namespace
+        try {
+            new TwitterData_Tuple('test>namespace', 'val');
+        } catch (UnexpectedValueException $e) {
+            $this->fail();
+        }
+
+        // incorrect namespace
+        try {
+            new TwitterData_Tuple('test>bad>namespace', 'val');
+            $this->fail();
+        } catch (UnexpectedValueException $e) {
+        }
+    }
+
+    public function testTupleValues()
+    {
+        $tuple = new TwitterData_Tuple('key', '$value');
+        $this->assertEquals('$key $$value', (string)$tuple);
+
+        $tuple = new TwitterData_Tuple('key', '  value');
+        $this->assertEquals('$key value', (string)$tuple);
+    }
+
+    public function testHighLevel()
+    {
+        $data = array('key' => 'value', 'key2' => 'value2');
+        $this->assertEquals('$key value $key2 value2', TwitterData::array_to_TwitterData($data));
+    }
+}
